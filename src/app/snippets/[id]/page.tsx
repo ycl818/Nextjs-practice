bb0c48e58b3cedae3d450eb22d0467c523444fda
yichien <kevinyichienlee@gmail.com>
2024-11-03 22:00:16 +0800

    Server actions in Nextjs Client components

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { db } from "@/db";
+import Link from "next/link";
 import { notFound } from "next/navigation";
+import * as actions from "@/actions";
 
 interface SnippetShowPageProps {
   params: {
@@ -8,7 +10,7 @@ interface SnippetShowPageProps {
 }
 
 export default async function SnippetsShowPage(props: SnippetShowPageProps) {
-  await new Promise((r) => setTimeout(r, 2000));
+  await new Promise((r) => setTimeout(r, 500));
 
   const snippet = await db.snippet.findFirst({
     where: { id: parseInt(props.params.id) },
@@ -16,5 +18,27 @@ export default async function SnippetsShowPage(props: SnippetShowPageProps) {
 
   if (!snippet) return notFound();
 
-  return <div>{snippet.title}</div>;
+  const deleteSnippetAction = actions.deleteSnipper.bind(null, snippet.id);
+
+  return (
+    <div>
+      <div className="flex m-4 justify-between items-center">
+        <h1 className="text-xl font-bold">{snippet.title}</h1>
+        <div className="flex gap-4">
+          <Link
+            href={`/snippets/${snippet.id}/edit`}
+            className="p-2 border rounded-s-none"
+          >
+            Edit
+          </Link>
+          <form action={deleteSnippetAction}>
+            <button className="p-2 border rounded-s-none">Delete</button>
+          </form>
+        </div>
+      </div>
+      <pre className="p-3 border rounded bg-gray-200 border-gray-200">
+        <code>{snippet.code}</code>
+      </pre>
+    </div>
+  );
 }
---
+++ src/components/snippet-edit-form.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import * as actions from "@/actions";
+import { Editor } from "@monaco-editor/react";
+import type { Snippet } from "@prisma/client";
+import { useState } from "react";
+
+interface SnippetEditFormProps {
+  snippet: Snippet;
+}
+
+export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
+  const [code, setCode] = useState(snippet.code);
+
+  const handleEditorChange = (value: string = "") => {
+    setCode(value);
+  };
+  const editSnippetAction = actions.editSnippet.bind(null, snippet.id, code);
+
+  return (
+    <div>
+      <Editor
+        height="40vh"
+        theme="vs-dark"
+        language="javascript"
+        defaultValue={snippet.code}
+        options={{ minimap: { enabled: false } }}
+        onChange={handleEditorChange}
+      />
+      <form action={editSnippetAction}>
+        <button type="submit" className="p-2 border rounded">
+          Save
+        </button>
+      </form>
+    </div>
+  );
+}
